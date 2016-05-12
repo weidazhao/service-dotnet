153858e3fbb85e51629cb76947918231980f8d18
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-05-12 12:33:28 -0700

    exp

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -9,7 +9,7 @@ namespace Backend
         [HttpGet]
         public string Get()
         {
-            return $"Hello ACS! Time: {DateTimeOffset.UtcNow.ToString("u")}";
+            return $"Hello Docker! Time: {DateTimeOffset.UtcNow.ToString("u")}";
         }
     }
 }
